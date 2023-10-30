4031846aed4076940d01ea2c357b8988f5796c87
Lony <loonydarck@hotmail.com>
2023-10-30 07:15:10 -0300

    Removendo pedidos do sistema

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -28,7 +28,7 @@
                         {{ s.tipo }}
                         </option>
                     </select>
-                    <button class="delete-btn">Cancelar</button>
+                    <button class="delete-btn" @click="deleteBurger(burger.id)">Cancelar</button>
                 </div>
             </div>
         </div>
@@ -53,8 +53,6 @@ export default {
 
             this.burgers = data;
 
-            console.log(this.burgers);
-
             //resgatar os status
 
                 this.getStatus();
@@ -65,6 +63,18 @@ export default {
             const data = await req.json();
 
             this.status = data;
+        },
+        async deleteBurger(id) {
+            
+            const req = await fetch(`http://localhost:3000/burgers/${id}`, {
+                method: "DELETE"
+            });
+
+            const res = await req.json();
+
+            //mensagem
+
+            this.getPedidos();
         }
     },
     mounted() {
